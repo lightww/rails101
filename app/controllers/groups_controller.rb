32279a8760a6d9e37c45b5ace4f1c0f150b0eb95
wayne <lightww@me.com>
2017-04-12 21:26:17 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy,:join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -44,6 +44,29 @@ class GroupsController < ApplicationController
     flash[:alert]= "Group deleted"
     redirect_to groups_path
   end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice]= "加入本讨论组成功"
+    else
+      flash[:warning]="你已是本讨论组成员"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:notice]= "退出本讨论组成功"
+    else
+      flash[:warning]="你不是本讨论组成员,怎么退出!"
+    end
+    redirect_to group_path(@group)
+  end
+
 
   private
 
